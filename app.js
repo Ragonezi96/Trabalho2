dd2b0eb5ac2fa2894cd3194911d28263a2124c60
Bernardo Ragonezi <bernardo_ragonezi@hotmail.com>
2020-12-05 23:49:37 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,193 @@
+// ===== Constantes
+
+const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZDJhZjg2Yzc2NWUwYzUzOTA3ZDY5OTM2MzI3ZDgxZSIsInN1YiI6IjVmY2MzNTkxYTU3NDNkMDAzZjA2N2RmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MHRxksSqi4yRxHehyxGhu7AJGeRjVD3rBr38LfbAYmw';
+
+const API_ROOT_URL = 'https://api.themoviedb.org/3/';
+const DEFAULT_PARAMS = 'language=pt-BR';
+
+const DEFAULT_HEADERS = {
+  'Authorization': `Bearer ${API_KEY}`,
+  'Content-Type': 'application/json;charset=utf-8'
+}
+
+
+// ===== Executor do código inicial
+$(document).ready(function () {
+  getTrending();
+  createSearchListener();
+});
+
+// ===== Injetores de conteúdo
+const getMovieCard = (data) => {
+  return `
+    <div class="card m-2" style="width: 25rem;">
+      <img class="card-img-top ${data.poster_path === null ? 'd-none' : ''}" src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt="${data.title}">
+      <div class="card-image-filler ${data.poster_path !== null ? 'd-none' : ''}"><span>Sem imagem</span></div>
+      <div class="card-body d-flex flex-column justify-content-between">
+        <h5 class="card-title">${data.name}</h5>
+        <p class="card-text">Pais(es) de origem   :   ${data.origin_country}</p>
+        <p class="card-text">Data de lançamento: ${data.first_air_date}</p>
+        <p class="card-text">Nota: ${data.vote_average}</p>
+        <a href="#" class="btn btn-red btn-movie-card" data-movie-id="${data.id}">Mais Informações</a>
+      </div>
+    </div>
+  `
+};
+
+const setModalContent = (data) => {
+  const imageNode = document.querySelector('#modalImage');
+
+  document.querySelector('#modalTitle').innerHTML = data.name;
+  document.querySelector('#modalSinopse').innerHTML = data.overview;
+
+  document.querySelector('#modalSite').href = data.homepage;
+  imageNode.src = `https://image.tmdb.org/t/p/w500${data.backdrop_path}`;
+
+  data.backdrop_path === null ? imageNode.classList.add('d-none') : imageNode.classList.remove('d-none');
+
+  const firstGenre = data.genres.pop();
+  let genreList = firstGenre.name;
+  data.genres.forEach(genre => {
+    genreList += `, ${genre.name}`;
+  })
+  document.querySelector('#modalGenero').innerHTML = genreList + '.';
+};
+
+
+// ===== Listeners
+const createCardListeners = () => {
+  const cardBtnList = document.querySelectorAll('.btn-movie-card');
+
+  for (const btnNode of cardBtnList) {
+    btnNode.addEventListener('click', ({ target }) => {
+      getSingleMovie(target.dataset.movieId);
+    })
+  }
+};
+
+const createSearchListener = () => {
+  document.querySelector('#searchBtnClick').addEventListener('click', () => {
+    const queryString = document.querySelector('#searchInput');
+    if (queryString.value.length < 3) {
+      window.alert('Você precisa inserir pelo menos 3 caracteres para buscar');
+    }
+    else {
+      getSearchResults(queryString.value);
+    }
+  });
+};
+
+
+
+const switchLoader = (loaderActive) => {
+  const loaderNode = document.querySelector('.loader');
+  loaderActive ? loaderNode.classList.remove('-off') : loaderNode.classList.add('-off');
+};
+
+
+
+const getTrending = async () => {
+  switchLoader(true);
+  await fetch(`${API_ROOT_URL}trending/tv/week?${DEFAULT_PARAMS}`, {
+    method: 'GET',
+    headers: DEFAULT_HEADERS
+  })
+    .then(async response => {
+      data = await response.json();
+      const movieListNode = document.querySelector('div.movie-list');
+
+      let movieCardString = '';
+      data.results.forEach(movie => {
+        movieCardString += getMovieCard(movie);
+      });
+
+      movieListNode.innerHTML = movieCardString;
+      createCardListeners();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  switchLoader(false);
+};
+
+const getSingleMovie = async (movieId) => {
+  switchLoader(true);
+  await fetch(`${API_ROOT_URL}tv/${movieId}?${DEFAULT_PARAMS}`, {
+    method: 'GET',
+    headers: DEFAULT_HEADERS
+  })
+    .then(async response => {
+      isSuccess = true;
+      data = await response.json();
+
+      setModalContent(data);
+      $('#movieDetailsModal').modal('show');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  switchLoader(false);
+};
+
+const getSearchResults = async (queryString) => {
+  switchLoader(true);
+  await fetch(`${API_ROOT_URL}search/movie/?query=${queryString}&${DEFAULT_PARAMS}&page=1&include_adult=false`, {
+    method: 'GET',
+    headers: DEFAULT_HEADERS
+  })
+    .then(async response => {
+      data = await response.json();
+      const movieListNode = document.querySelector('div.movie-list');
+
+      let movieCardString;
+      if (data.total_results > 20) {
+        movieCardString = `
+          <div class="w-100 text-center mb-3">
+            <strong>${data.total_results} resultados encontrados, porém exibindo apenas 20. Refine sua busca para encontrar algo mais específico.</strong>
+            <br>Você também pode voltar para a lista dos mais populares <a href="/portal-filmes/">clicando aqui.</a>
+          </div>
+          `;
+      }
+      else {
+        movieCardString = `
+          <div class="w-100 text-center mb-3">
+            <strong>Exibindo todos os ${data.total_results} resultados encontrados.</strong>
+            <br>Você também pode voltar para a lista dos mais populares <a href="/portal-filmes/">clicando aqui.</a>
+          </div>
+          `;
+      }
+
+      let resultsFound = false;
+      data.results.forEach(movie => {
+        resultsFound = true;
+        movieCardString += getMovieCard(movie);
+      });
+
+      if (!resultsFound) {
+        movieListNode.innerHTML = `
+        <div class="w-100 text-center font-weight-bold">Nada foi encontrado. Tente novamente ou volte para a home <a href="/portal-filmes/">clicando aqui.</a></div>
+        `;
+      }
+      else {
+        movieListNode.innerHTML = movieCardString;
+        createCardListeners();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      const movieListNode = document.querySelector('div.movie-list');
+      const movieCardString = `
+        <div class="w-100 text-center mb-3">
+          <strong>Houve um erro na requisição da busca. O TMDb está redirecionando os requests de busca para HTTP. Como o navegador detecta esse comportamento compo inseguro, a requisição foi bloqueada. Para contornar esse problema no sistema do TMDb, você deve baixar o repositório e rodá-lo localmente</strong>
+          <br>O repositório está disponível publicamente <a href="https://github.com/pud1m/portal-filmes" target="_blank">neste link.</a>
+          <br>Você também pode voltar para a lista dos mais populares <a href="/portal-filmes/">clicando aqui.</a>
+        </div>
+        `;
+      movieListNode.innerHTML = movieCardString;
+      window.alert('Erro ocasionado por problema na API do TMDb: ' + err);
+    });
+
+  switchLoader(false);
+};
